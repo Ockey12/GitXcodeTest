26c8196435488359ebed3b6a43f082181d5fcae8
OnagaHaruki <haloupgarou@gmail.com>
2022-03-26 11:24:05 +0900

    Initial Commit

---
+++ GitXcodeTest/GitXcodeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitXcodeTestApp.swift
+//  GitXcodeTest
+//
+//  Created by オナガ・ハルキ on 2022/03/26.
+//
+
+import SwiftUI
+
+@main
+struct GitXcodeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
