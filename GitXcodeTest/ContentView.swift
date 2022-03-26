ce16013a7514b6508b07133ca218c4e8bd4e89a2
OnagaHaruki <haloupgarou@gmail.com>
2022-03-26 12:04:02 +0900

    Add text "branch1 merged into main".

--- GitXcodeTest/ContentView.swift
+++ GitXcodeTest/ContentView.swift
@@ -16,6 +16,9 @@ struct ContentView: View {
             Text("branch1")
                 .bold()
                 .font(.system(size: 100))
+            
+            Text("branch1 merged into main")
+                .bold()
         }
     }
 }
